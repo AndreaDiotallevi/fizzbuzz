fb9ec9c292a68213a7b816f4304f0aabcffc2100
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-10-27 13:55:04 +0000

    try to add edge cases

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -2,6 +2,7 @@ describe 'Fizzbuzz' do
   context "If number is divisible by 3 and not by 5" do
       it 'returns "fizz"' do
         expect(fizzbuzz(9)).to eq "fizz"
+        expect(fizzbuzz(13)).not_to eq "fizz"
       end
   end
 
@@ -14,12 +15,14 @@ describe 'Fizzbuzz' do
   context "If number is divisible by 3 and 5" do
     it 'returns "fizzbuzz"' do
         expect(fizzbuzz(15)).to eq "fizzbuzz"
+        expect(fizzbuzz(0)).to eq "fizzbuzz"
     end
   end
 
   context "If number is not divisible by neither 3 or 5" do
     it 'returns given number' do
         expect(fizzbuzz(2)).to eq 2
+        expect(fizzbuzz(0)).not_to eq 0
     end
   end
 end
